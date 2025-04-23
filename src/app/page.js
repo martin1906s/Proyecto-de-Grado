f5107c902d40918965f64f924a914b3c6b0febc5
martin1906s <martin.simbana007@gamil.com>
2025-04-22 23:19:01 -0500

    Corrección de faltas ortográficas

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Main() {
         </h1>
         <p>
           <b>NeuroGame</b> es un innovador videojuego diseñado para apoyar la rehabilitación de personas que han sufrido daños cerebrales o en sus manos. Su enfoque combina entretenimiento y terapia, proporcionando una experiencia inmersiva que estimula y refuerza habilidades cognitivas y motoras esenciales para la recuperación.
-          A través de diversas dinámicas de juego, <b>NeuroGame</b>NeuroGame ayuda a mejorar la memoria, la coordinación, la atención y la resolución de problemas. Cada desafío está diseñado con base en principios de neuroplasticidad, permitiendo que los jugadores progresen a su propio ritmo mientras reciben retroalimentación inmediata sobre su desempeño.
+          A través de diversas dinámicas de juego, <b>NeuroGame</b> ayuda a mejorar la memoria, la coordinación, la atención y la resolución de problemas. Cada desafío está diseñado con base en principios de neuroplasticidad, permitiendo que los jugadores progresen a su propio ritmo mientras reciben retroalimentación inmediata sobre su desempeño.
           Además, su interfaz accesible y adaptable permite que personas con distintos niveles de movilidad puedan interactuar con facilidad. <b>NeuroGame</b> no solo ofrece una herramienta de rehabilitación, sino que también fomenta la motivación y la superación personal, convirtiendo el proceso de recuperación en una experiencia divertida y estimulante.</p>
         <Images />
         <Download />
